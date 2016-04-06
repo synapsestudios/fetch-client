92bd7daaace027856dce2f1f859ba5d785e2a259
spruce-bruce <aaron@synapsestudios.com>
2016-04-06 15:50:27 -0700

    using all the complicated things

--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
-const assert = require('assert');
+const chai = require('chai');
+const sinon = require('sinon');
+const rewire = require('rewire');
+const expect = chai.expect;
+
+const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
 describe('example', () => {
   it('should pass', () => {
-    assert.equal(1, 1);
+    expect(1).to.equal(1);
   });
 });
