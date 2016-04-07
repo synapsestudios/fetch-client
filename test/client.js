bea9c62144dfab9751867371352172def6a49b12
spruce-bruce <aaron@synapsestudios.com>
2016-04-06 21:26:23 -0700

    adding tests for event emitter

--- test/client.js
+++ test/client.js
@@ -1,4 +1,4 @@
-/* eslint no-unused-vars:0 */
+/* eslint no-unused-vars:0, no-unused-expressions:0 */
 const chai = require('chai');
 const sinon = require('sinon');
 const rewire = require('rewire');
@@ -10,9 +10,40 @@ chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
 const Client = require('../src/client');
+const Request = require('whatwg-fetch').Request;
 
 describe('client', () => {
   it('should not fail to instantiate', () => {
     const myClient = new Client({ arbitrary: 'object' });
+  });
+
+  describe('events', () => {
+    it('should emit starting event', () => {
+      const myClient = new Client();
+      const cb = sinon.spy();
+      myClient.on('request_start', cb);
+
+      myClient.fetch('http://google.com/', { method: 'get' });
+      expect(cb).to.have.been.calledOnce;
+      expect(cb.args[0][0]).to.be.instanceof(Request);
+      expect(cb.args[0][0]).to.have.property('url', 'http://google.com/');
+      expect(cb.args[0][0]).to.have.property('method', 'GET');
+    });
+
+    it('should emit success event', () => {
+      const myClient = new Client();
+      const cb = sinon.spy();
+      myClient.on('request_success', cb);
+
+      const promise = myClient.fetch('http://google.com/', { method: 'get' });
+
+      return expect(promise).to.be.fulfilled.then(x => {
+        expect(cb).to.have.been.calledOnce;
+      });
+    });
+
+    it('should emit fail event', () => {
+
+    });
   });
 });
