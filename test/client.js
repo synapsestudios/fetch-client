21b213d6f4b33b2f84ac27c393edb8101e7664b8
spruce-bruce <aaron@synapsestudios.com>
2016-04-06 21:55:08 -0700

    more event emitter tests

--- test/client.js
+++ test/client.js
@@ -9,8 +9,9 @@ const sinonChai = require('sinon-chai');
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
-const Client = require('../src/client');
+const Client = rewire('../src/client');
 const Request = require('whatwg-fetch').Request;
+var EventEmitter2 = require('eventemitter2');
 
 describe('client', () => {
   it('should not fail to instantiate', () => {
@@ -31,6 +32,8 @@ describe('client', () => {
     });
 
     it('should emit success event', () => {
+      Client.__set__('fetch', (request) => Promise.resolve('test'));
+
       const myClient = new Client();
       const cb = sinon.spy();
       myClient.on('request_success', cb);
@@ -39,11 +42,34 @@ describe('client', () => {
 
       return expect(promise).to.be.fulfilled.then(x => {
         expect(cb).to.have.been.calledOnce;
+        expect(cb.args[0][0]).to.be.instanceof(Request);
+        expect(cb.args[0][0]).to.have.property('url', 'http://google.com/');
+        expect(cb.args[0][0]).to.have.property('method', 'GET');
+        expect(cb.args[0][1]).to.equal('test');
       });
     });
 
     it('should emit fail event', () => {
+      Client.__set__('fetch', (request) => Promise.reject('test'));
 
+      const myClient = new Client();
+      const cb = sinon.spy();
+      myClient.on('request_fail', cb);
+
+      const promise = myClient.fetch('http://google.com/', { method: 'get' });
+
+      return expect(promise).to.be.rejected.then(x => {
+        expect(cb).to.have.been.calledOnce;
+        expect(cb.args[0][0]).to.be.instanceof(Request);
+        expect(cb.args[0][0]).to.have.property('url', 'http://google.com/');
+        expect(cb.args[0][0]).to.have.property('method', 'GET');
+        expect(cb.args[0][1]).to.equal('test');
+      });
+    });
+
+    it('should return an event emitter object', () => {
+      const myClient = new Client();
+      expect(myClient.eventEmitter).to.be.instanceof(EventEmitter2);
     });
   });
 });
