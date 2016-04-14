55f5d445a22b5f78a60578ed6e9967402bfa5edc
Zachary Chavez <zach@synapsestudios.com>
2016-04-14 09:24:54 -0700

    Add additional resumable-uploader tests

--- test/resumable-uploader-test.js
+++ test/resumable-uploader-test.js
@@ -10,23 +10,31 @@ chai.use(sinonChai);
 
 import Uploader from '../src/resumable-uploader';
 import EventEmitter2 from 'eventemitter2';
-import * as events from '../lib/events';
+import * as events from '../src/events';
 import { Request, fetch } from 'whatwg-fetch';
 GLOBAL.Request = Request;
 GLOBAL.File = require('file-api').File;
 
 describe('uploader', () => {
-  const mockResumable = function MockResumable() {
-    this.support = true;
-    this.addFile = () => {};
-    this.on = () => {};
-  };
-  Uploader.__Rewire__('Resumable', mockResumable);
-
   let uploader;
+  let mockResumableEventEmitter;
+  let uploadedFile;
   describe('events', () => {
     beforeEach(() => {
-      uploader = new Uploader('/api-path', new GLOBAL.File('./uploader-test.js'));
+      mockResumableEventEmitter = new EventEmitter2();
+      const MockResumable = function mockRes() {
+        this.support = true;
+        this.addFile = () => {};
+        this.on = mockResumableEventEmitter.on.bind(mockResumableEventEmitter);
+        this.emit = mockResumableEventEmitter.emit.bind(mockResumableEventEmitter);
+      };
+      uploadedFile = new GLOBAL.File('./uploader-test.js');
+      Uploader.__Rewire__('Resumable', MockResumable);
+      uploader = new Uploader('/api-path', uploadedFile);
+    });
+
+    afterEach(() => {
+      Uploader.__ResetDependency__('Resumable');
     });
 
     it('should emit starting event', () => {
@@ -36,6 +44,45 @@ describe('uploader', () => {
       uploader.upload().then(() => {
         expect(cb).to.have.been.calledOnce;
       });
+    });
+
+    it('should emit success event on fileSuccess event', () => {
+      const cb = sinon.spy();
+
+      uploader.on(events.REQUEST_SUCCESS, cb);
+
+      const promise = uploader.upload();
+      mockResumableEventEmitter.emit('fileSuccess', 'file', '"message"');
+
+      return expect(promise).to.be.fulfilled.then(() => {
+        expect(cb).to.have.been.calledOnce;
+      });
+    });
+
+    it('should emit failure event', () => {
+      const cb = sinon.spy();
+
+      uploader.on(events.REQUEST_FAILURE, cb);
+
+      const promise = uploader.upload();
+      mockResumableEventEmitter.emit('fileError', 'file', '"message"');
+
+      return expect(promise).to.be.rejected.then(() => {
+        expect(cb).to.have.been.calledOnce;
+      });
+    });
+
+    it('should emit upload progress events', () => {
+      const cb = sinon.spy();
+
+      uploader.on(events.UPLOAD_PROGRESS, cb);
+
+      const promise = uploader.upload();
+      uploadedFile.progress = () => 0.50;
+      mockResumableEventEmitter.emit('fileProgress', uploadedFile);
+
+      expect(cb).to.have.been.calledOnce;
+      expect(cb).to.have.been.calledWith(50);
     });
   });
 });
