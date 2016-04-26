5eb8f777801a275b87005ae487a57217bfa4ff00
Zachary Chavez <zach@rychavez.com>
2016-04-26 15:31:48 -0700

    Fix linter issue

--- src/resumable-uploader.js
+++ src/resumable-uploader.js
@@ -4,7 +4,6 @@ import Client from './client';
 
 export default class ResumableUploader extends Client {
   upload(path, fileToUpload, _options = {}) {
-
     // Set defaults
     const options = Object.assign(
       {},
