46f731446a7086744f8ea6316825a04a078530f7
Zachary Chavez <zach@synapsestudios.com>
2016-05-11 15:00:00 -0700

    Export events and PluginError

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import Client from './client';
+import * as events from './events';
 import jwtPlugin from './plugins/jwt';
+import PluginError from './plugin-error';
 
-export { jwtPlugin };
+export {
+  events,
+  jwtPlugin,
+  PluginError,
+};
 
 export default Client;
