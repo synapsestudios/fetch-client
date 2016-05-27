b5fd52605a37b51aa00a269ba73a43f7e3994aba
Zachary Chavez <zach@synapsestudios.com>
2016-05-26 17:10:32 -0700

    Some WIP on the plugin. Still figuring things out.

---
+++ src/plugins/oauth.js
@@ -0,0 +1,51 @@
+const TOKEN_REFRESHED = 'TOKEN_REFRESHED';
+
+export default {
+  onStart(request) {
+    if (this.refreshing) {
+      request.waitPromise = new Promise(resolve => {
+        request.headers.append('Authorization', `Bearer ${this.client.getBearerToken()}`);
+        this.client.eventEmitter.on(TOKEN_REFRESHED, resolve());
+      });
+    } else {
+      request.headers.append('Authorization', `Bearer ${this.client.getBearerToken()}`);
+    }
+    return request;
+  },
+
+  onComplete(request, response) {
+    if (response.status === 401) {
+      this.refreshing = true;
+      this.retryAfterRefresh.push(request);
+      response.doOver = this.client.refreshToken().then(() => {
+        this.client.eventEmitter.emit(TOKEN_REFRESHED);
+      });
+    }
+  },
+
+  helpers: {
+    setConfig(config) {
+      this.oauthConfig = config;
+    },
+
+    refreshToken() {
+      return this.client.fetch(this.client.oauthConfig.refresh_path, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `grant_type=refresh_token&refresh_token=${this.client.getRefreshToken()}`,
+      });
+    },
+
+    setBearerTokenGetter(func) {
+      this.getBearerToken = func;
+    },
+
+    getBearerToken() {
+      throw new Error(
+        'You must define getBearerToken with client.helpers.setBearerTokenGetter'
+      );
+    },
+  },
+};
