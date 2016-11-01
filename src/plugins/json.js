044c61394713ef7316a134c32f7715fdac2c1162
Gordon Forsythe <baohx2000@gmail.com>
2016-11-01 13:22:43 -0700

    properly check for json response (#44)
    * properly check for json response
    
    * also parse on error
    
    * cleanup
    
    * cleanup
    
    * add comma for linter
    
    * Add test

--- src/plugins/json.js
+++ src/plugins/json.js
@@ -1,8 +1,18 @@
 export default {
-  onSuccess(request, response) {
+  onSuccess: (request, response) => {
     response.parsedBody = () => {
       const contentType = response.headers.get('Content-Type') || '';
-      if (contentType === 'application/json') {
+      if (contentType.match(/application\/json/)) {
+        return response.json();
+      }
+      return response.text();
+    };
+    return response;
+  },
+  onFail: (request, response) => {
+    response.parsedBody = () => {
+      const contentType = response.headers.get('Content-Type') || '';
+      if (contentType.match(/application\/json/)) {
         return response.json();
       }
       return response.text();
